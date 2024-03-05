10dc27888da0626e8e38c2b1343c9dc919a88d8a
somramnani <som.ramnani@gmail.com>
2024-03-05 15:20:53 -0500

    add hero image and content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,40 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div></div>;
+  return (
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* Image container */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image
+          src="/meAndDogArtsy.png"
+          alt=""
+          fill
+          className="object-contain"
+        />
+      </div>
+      {/* Text container */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        {/* Title */}
+        <h1 className="text-4xl md:text-6xl font-bold">Welcome</h1>
+        <p className="md:text-xl">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </p>
+        <div className="flex gap-4">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            View My Work
+          </button>
+          <button className="p-4 rounded-lg ring-1 ring-black">
+            Contact Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
