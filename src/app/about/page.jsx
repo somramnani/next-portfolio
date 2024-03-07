53dc4c053ad02e7ccf0471c1e0abaaed8c5e665e
somramnani <som.ramnani@gmail.com>
2024-03-07 11:35:09 -0500

    add Skills section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -16,7 +16,7 @@ const AboutPage = () => {
         <div className=" ">
           <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
             <div className="flex flex-col gap-12 justify-center">
-              <h1 className="font-bold text-2">BIOGRRAPHY</h1>
+              <h1 className="font-bold text-2">BIOGRAPHY</h1>
               <p classname="text-lg">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
                 omnis eum atque molestiae eligendi exercitationem impedit
@@ -44,7 +44,107 @@ const AboutPage = () => {
                 </svg>
               </div>
             </div>
-            <div className="font-bold">SKILLS</div>
+            <div className="flex flex-col gap-12 justify-center">
+              <h1 className="font-bold text-2">SKILLS</h1>
+              <div className="flex gap-4 flex-wrap">
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  HTML5
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  CSS3
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  JavaScript
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Java
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  React.js
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Node.js
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Express.js
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  jQuery
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Passport
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  TypeScript
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Storybook
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Cypress
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Jest
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Mocha
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Jasmine
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Chai
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  React.js
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  GraphQL
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Sequelize
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Bootstrap
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Heroku
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Git
+                </div>
+              </div>
+
+              <div className=""></div>
+
+              <motion.svg
+                initial={{ opacity: 0.2, y: 0 }}
+                animate={{ opacity: 1, y: "10px" }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 3,
+                  ease: "easeInOut",
+                }}
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                width={50}
+                height={50}
+              >
+                <path
+                  d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                  stroke="#000000"
+                  strokeWidth="1"
+                ></path>
+                <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+                <path
+                  d="M15 11L12 14L9 11"
+                  stroke="#000000"
+                  strokeWidth="1"
+                ></path>
+              </motion.svg>
+            </div>
+
             <div className="">EXPERIENCE</div>
           </div>
           <div className="hidden"></div>
