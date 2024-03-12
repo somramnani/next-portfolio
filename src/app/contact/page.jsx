3f32cde28196472c8eaf4a1196b327c44c6ea842
somramnani <som.ramnani@gmail.com>
2024-03-12 19:27:52 -0400

    add framer motion text typing animation

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,7 @@
 import { motion } from "framer-motion";
 
 const ContactPage = () => {
+  const text = "Say Hello";
   return (
     <div>
       {" "}
@@ -12,7 +13,31 @@ const ContactPage = () => {
         animate={{ y: "0%" }}
         transition={{ duration: 1 }}
       >
-        Contact Page
+        <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
+          {/* Text Container */}
+          <div className="h-1/2 lg:h-full lg:w-1/2">
+            <motion.div className="div">
+              {text.split("").map((letter, index) => {
+                return (
+                  <motion.span
+                    key={index}
+                    initial={{ opacity: 1 }}
+                    animate={{ opacity: 0 }}
+                    transition={{
+                      duration: 3,
+                      repeat: Infinity,
+                      delay: index * 0.1,
+                    }}
+                  >
+                    {letter}
+                  </motion.span>
+                );
+              })}
+            </motion.div>
+          </div>
+          {/* Form Container */}
+          <form className="h-1/2 lg:h-full lg:w-1/2"></form>
+        </div>
       </motion.div>
     </div>
   );
