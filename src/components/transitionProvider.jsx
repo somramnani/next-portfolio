7f49839a396d50232c4e46f78825ad2a580310d0
somramnani <som.ramnani@gmail.com>
2024-03-15 16:10:04 -0400

    edit navbar route in transitionProvider

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { AnimatePresence } from "framer-motion";
-import Navbar from "@/components/navbar";
+import Navbar from "./navbar";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
