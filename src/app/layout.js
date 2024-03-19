0aea8caa6bacdd2e8fb3eb1b6651ec6e0977b93b
somramnani <som.ramnani@gmail.com>
2024-03-19 12:52:28 -0400

    add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TransitionProvider>{children}</TransitionProvider>
+        <TransitionProvider>
+          {children}
+          <Analytics />
+        </TransitionProvider>
       </body>
     </html>
   );
